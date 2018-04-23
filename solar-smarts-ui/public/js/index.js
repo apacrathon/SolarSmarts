b8fccd2688422d3d994b95ea5a56a49446b1e8d0
prabh961 <prabh961@gmail.com>
2018-04-23 14:56:38 -0400

    more ui updates. TODO: Anomaly table in UI and database

--- solar-smarts-ui/public/js/index.js
+++ solar-smarts-ui/public/js/index.js
@@ -37,6 +37,7 @@ app.controller('myCtrl', [
             $( function() {
                 $( "#datepicker" ).datepicker({
                     changeMonth: true,
+                    defaultDate: "2016-06-01",
                     changeYear: true,
                     onSelect: function() {
                         todayPrediction = document.getElementById('datepicker').value;
@@ -46,6 +47,8 @@ app.controller('myCtrl', [
 
                 $( "#datepicker" ).datepicker( "option", "dateFormat", 'yy-mm-dd' );
             } );
+            var myLineChart;
+            var mySolarChart;
             function populateFields(todayPrediction) {
                 $scope.$apply(() => {
                     $scope.datePicked = 1;
@@ -84,10 +87,7 @@ app.controller('myCtrl', [
                         $scope.irradianceAvg = irradianceAvg;
                         $scope.humidityAvg = humidityAvg;
                         $scope.tempAvg = tempAvg;
-                        if(response[0].length == 0) {
-                            console.log("jiro");
-                            $scope.datePicked = 2;
-                        }
+
                     });
                 });
                 // Current Day Graph and statistics
@@ -102,24 +102,49 @@ app.controller('myCtrl', [
                     let actualEnergy = 0;
                     let pEnergyArr = [];
                     let aEnergyArr = [];
+                    let pIrradianceArr = [];
+                    let nIrradianceArr = [];
+                    let irradianceSum = 0;
+                    let humiditySum = 0;
+                    let tempSum = 0;
 
                     for(var i = 0; i < response[0].length; i++) {
                         predictedEnergy += response[0][i].PGenerated;
-                        pEnergyArr[i] = response[0][i].PGenerated;
+                        pEnergyArr[i] = (response[0][i].PGenerated).toFixed(2);
                         actualEnergy += response[0][i].NGenerated;
-                        aEnergyArr[i] = response[0][i].NGenerated;
+                        aEnergyArr[i] = (response[0][i].NGenerated).toFixed(2);
+                        irradianceSum += response[0][i].PSolar;
+                        pIrradianceArr[i] = (response[0][i].PSolar).toFixed(2);
+                        nIrradianceArr[i] = (response[0][i].NSolar).toFixed(2);
+                        humiditySum += response[0][i].PHumidity;
+                        tempSum += response[0][i].PTemp;
                         let tableTime = new Date(response[0][i].PDate);
                         response[0][i].timeOfDay = "" + tableTime.getUTCHours() + ":" + tableTime.getUTCMinutes() + "0";
                         let percentError = Math.abs((response[0][i].NGenerated-response[0][i].PGenerated)/response[0][i].NGenerated)*100;
                         percentError = percentError.toFixed(2);
                         response[0][i].percentError = percentError;
+                        response[0][i].PGenerated = (response[0][i].PGenerated).toFixed(2);
+                        response[0][i].NGenerated = (response[0][i].NGenerated).toFixed(2);
+
                     }
+                    console.log(pEnergyArr);
+                    let irradianceAvg = Math.round(irradianceSum/response[0].length);
+                    let humidityAvg = Math.round((humiditySum*100)/response[0].length);
+                    let tempAvg = Math.round(tempSum/response[0].length);
                     let maxEnergy = Math.max(Math.max(...pEnergyArr),Math.max(...aEnergyArr));
+                    let maxSolar = Math.max(Math.max(...pIrradianceArr),Math.max(...nIrradianceArr));
                     $scope.$apply(() => {
-                        $scope.PEnergyGenerated = predictedEnergy;
-                        $scope.AEnergyGenerated = actualEnergy;
+                        $scope.PEnergyGenerated = predictedEnergy.toFixed(0);
+                        $scope.AEnergyGenerated = actualEnergy.toFixed(0);
                         $scope.todayPrediction = todayPrediction;
                         $scope.energyGenPredTable = response[0];
+                        $scope.irradianceAvg = irradianceAvg;
+                        $scope.humidityAvg = humidityAvg;
+                        $scope.tempAvg = tempAvg;
+                        if(response[0].length == 0) {
+                            console.log("jiro");
+                            $scope.datePicked = 2;
+                        }
 
                         $scope.currentPage = 1;
                         $scope.total_show = "15";
@@ -141,7 +166,13 @@ app.controller('myCtrl', [
 
 
                         var ctx = document.getElementById("myAreaChart");
-                        var myLineChart = new Chart(ctx, {
+                        if(myLineChart){
+                            myLineChart.destroy();
+                        }
+                        if(mySolarChart) {
+                            mySolarChart.destroy();
+                        }
+                        myLineChart = new Chart(ctx, {
                             type: 'line',
                             data: {
                                 labels: ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"],
@@ -197,12 +228,86 @@ app.controller('myCtrl', [
                                     yAxes: [{
                                         ticks: {
                                             min: 0,
-                                            max: maxEnergy+100,
+                                            max: Math.ceil((maxEnergy+100)/100)*100,
                                             maxTicksLimit: Math.ceil(maxEnergy/50)
                                         },
                                         scaleLabel: {
                                             display: true,
                                             labelString: 'kWh Produced'
+                                        },
+                                        gridLines: {
+                                            color: "rgba(0, 0, 0, .125)",
+                                        }
+                                    }],
+                                },
+                                legend: {
+                                    display: true
+                                }
+                            }
+                        });
+                        var ctx2 = document.getElementById("mySolarChart");
+                        mySolarChart = new Chart(ctx2, {
+                            type: 'line',
+                            data: {
+                                labels: ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"],
+                                datasets: [
+                                    {
+                                        label: "Predicted",
+                                        lineTension: 0.3,
+                                        backgroundColor: "rgba(2,117,216,0.2)",
+                                        borderColor: "rgba(2,117,216,1)",
+                                        pointRadius: 5,
+                                        pointBackgroundColor: "rgba(2,117,216,1)",
+                                        pointBorderColor: "rgba(255,255,255,0.8)",
+                                        pointHoverRadius: 5,
+                                        pointHoverBackgroundColor: "rgba(2,117,216,1)",
+                                        pointHitRadius: 20,
+                                        pointBorderWidth: 2,
+                                        data: pIrradianceArr,
+                                    },
+                                    {
+                                        label: "Actual",
+                                        lineTension: 0.3,
+                                        backgroundColor: "rgba(216,45,2,0.2)",
+                                        borderColor: "rgba(216,45,2,1)",
+                                        pointRadius: 5,
+                                        pointBackgroundColor: "rgba(216,45,2,1)",
+                                        pointBorderColor: "rgba(216,45,2,0.8)",
+                                        pointHoverRadius: 5,
+                                        pointHoverBackgroundColor: "rgba(216,45,2,1)",
+                                        pointHitRadius: 20,
+                                        pointBorderWidth: 2,
+                                        data: nIrradianceArr,
+                                    }
+                                ],
+
+                            },
+                            options: {
+                                scales: {
+                                    xAxes: [{
+                                        scaleLabel: {
+                                            display: true,
+                                            labelString: 'Hours'
+                                        },
+                                        time: {
+                                            unit: 'date'
+                                        },
+                                        gridLines: {
+                                            display: false
+                                        },
+                                        ticks: {
+                                            maxTicksLimit: 13
+                                        }
+                                    }],
+                                    yAxes: [{
+                                        ticks: {
+                                            min: 0,
+                                            max: Math.ceil((maxSolar+100)/100)*100,
+                                            maxTicksLimit: Math.ceil(maxSolar/50)
+                                        },
+                                        scaleLabel: {
+                                            display: true,
+                                            labelString: 'Solar Irradiance'
                                         },
                                         gridLines: {
                                             color: "rgba(0, 0, 0, .125)",
@@ -233,14 +338,14 @@ app.controller('myCtrl', [
                     let percentError = Math.abs((actualEnergy-predictedEnergy)/actualEnergy)*100;
                     percentError = percentError.toFixed(2);
                     $scope.$apply(() => {
-                        $scope.pEnergyGeneratedYday = predictedEnergy;
-                        $scope.aEnergyGeneratedYday = actualEnergy;
+                        $scope.pEnergyGeneratedYday = predictedEnergy.toFixed(0);
+                        $scope.aEnergyGeneratedYday = actualEnergy.toFixed(0);
                         $scope.ydayPercentError = percentError;
 
 
                     });
                 });
-
+                //past week
                 query.find({
                     query: {
                         rawQuery: "SELECT P.Date as PDate, P.Temperature as PTemp, P.Humidity as PHumidity, P.Solar_Irradiance as PSolar, P.ghi as PGhi, P.Power_generated as PGenerated, N.Date as NDate, N.Solar_Irradiance as NSolar, N.Temperature as NTemp, N.Power_Generation as NGenerated FROM SolarSmarts.Prediction P, SolarSmarts.Noveda N WHERE ((P.Date = N.Date) AND P.Date between '"+pastWeek+"' AND '"+todayPrediction+" 23:59:59') GROUP BY P.Date;"
